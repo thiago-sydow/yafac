30de85df91139a04bafa6f10041bfffd8f785df8
Thiago von Sydow <thiagovs05@gmail.com>
2016-07-03 12:38:21 -0300

    basic spec config

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'rspec'
+require 'rack/test'
+require 'byebug'
+
+ENV['RACK_ENV'] = 'test'
+
+# pull in the VCR setup
+require File.expand_path './support/vcr.rb', __dir__
+
+# Requiring api files
+require "./lib/yafac/main.rb"
+
+RSpec.configure do |config|
+
+  # Disable usage of monkey patch version of describe
+  # by preventing global injection of RSpec's DSL
+  config.expose_dsl_globally = false
+end
---
+++ spec/support/vcr.rb
@@ -0,0 +1,10 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+
+  # Default cassette options
+  c.default_cassette_options = { :record => :new_episodes }
+end
