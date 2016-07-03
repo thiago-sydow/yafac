e023db855b1e50838434e185afa844ad7b216a37
Thiago von Sydow <thiagovs05@gmail.com>
2016-07-03 13:28:05 -0300

    added timestamp check scenario

--- spec/lib/yafac/offer_client_spec.rb
+++ spec/lib/yafac/offer_client_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Yafac::OfferClient do
     it 'merges the informed attributes into params' do
       expect(client.instance_variable_get(:@params)).to include(informed)
     end
+
+    it 'adds a timestamp key to params' do
+      expect(client.instance_variable_get(:@params)).to include(:timestamp)
+    end
   end
 
 end
