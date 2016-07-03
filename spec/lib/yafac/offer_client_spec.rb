263595bd47bae89e44a5b24525e183a935ebd71b
Thiago von Sydow <thiagovs05@gmail.com>
2016-07-03 13:25:13 -0300

    added offer_client specs

---
+++ spec/lib/yafac/offer_client_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Yafac::OfferClient do
+
+  describe '#initialize' do
+    let(:informed) { {pub0: 'x', uid: 'y', page: '1'} }
+    let(:client) { Yafac::OfferClient.new(informed) }
+
+    it 'merges the informed attributes into params' do
+      expect(client.instance_variable_get(:@params)).to include(informed)
+    end
+  end
+
+end
