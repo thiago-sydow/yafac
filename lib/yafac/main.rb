d0a6a0d46019f40a2ec2f39f3cbbee0b7a78878e
Thiago von Sydow <thiagovs05@gmail.com>
2016-07-03 12:37:52 -0300

    basic sinatra configs added

---
+++ lib/yafac/main.rb
@@ -0,0 +1,34 @@
+require 'sinatra/base'
+
+module Yafac
+  class Main < Sinatra::Base
+
+    get '/' do
+      ''
+    end
+
+    post '/offers' do
+      ''
+    end
+
+    configure do
+      # Disable unused features to speed up
+      disable :method_override
+      disable :static
+
+      # Hide exception from users
+      set show_exceptions: false
+      set :dump_errors, false
+
+      # Set app's root
+      set :root, File.dirname(__FILE__)
+      enable :logging
+    end
+
+    error do
+      status 500
+      "Unknown error. " + env['sinatra.error'].message
+    end
+
+  end
+end
